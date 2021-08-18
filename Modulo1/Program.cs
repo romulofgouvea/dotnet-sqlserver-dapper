0dedf7964568394e339b0128195046eab18ee5e7
romulofgouvea <romulofgouvea@gmail.com>
2021-08-17 21:30:22 -0300

    Criando o modulo 1

---
+++ Modulo1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Modulo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main");
+            
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Dotnet 5 com SQLServer e Dapper
+
+Antes de tudo ver a configuração do SQLServer no docker [Link](https://beta.balta.io/blog/sql-server-docker)
