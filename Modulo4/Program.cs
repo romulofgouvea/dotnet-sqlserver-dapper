b4dc8a63a56ba797ee2aee46720568b77cd2eacb
romulofgouvea <romulofgouvea@gmail.com>
2021-08-23 22:22:55 -0300

    Modificando o codigo para o padrão repository

--- Modulo4/Program.cs
+++ Modulo4/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 using Dapper.Contrib.Extensions;
 using Modulo4.Models;
+using Modulo4.Repositories;
 using System;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace Modulo4
 {
@@ -14,60 +16,24 @@ namespace Modulo4
         {
             Console.WriteLine("Módulo 4 - Mão na massa");
 
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
+            using var connection = new SqlConnection(CONNECTION_STRING);
 
-                //ReadUsers(connection);
-                //ReadUser(connection, 2);
-                //var user = new User
-                //{
-                //    Bio = "a",
-                //    Email = "a@a.com",
-                //    Image = "https://",
-                //    Name = "Aaa",
-                //    PasswordHash = "Pass",
-                //    Slug = "sluga"
-                //};
-                //InsertUser(connection, user);
-            }
+            var repository = new UserRepository(connection);
+            repository.GetAll()
+                .ToList()
+                .ForEach(user => Console.WriteLine(user.Name));
+
+            // repository.Get(1);
+            //var user = new User
+            //{
+            //    Bio = "a",
+            //    Email = "a@a.com",
+            //    Image = "https://",
+            //    Name = "Aaa",
+            //    PasswordHash = "Pass",
+            //    Slug = "sluga"
+            //};
+            //InsertUser(connection, user);
         }
-
-        static void ReadUsers(SqlConnection connection)
-        {
-            var users = connection.GetAll<User>();
-
-            foreach (var user in users)
-            {
-                Console.WriteLine(user.Name);
-            }
-        }
-
-        static void ReadUser(SqlConnection connection, int id)
-        {
-            var user = connection.Get<User>(id);
-            Console.WriteLine(user.Name);
-
-        }
-
-        static void InsertUser(SqlConnection connection, User user)
-        {
-            connection.Insert(user);
-            Console.WriteLine("Cadastro realizado com sucesso!");
-        }
-
-        static void UpdateUser(SqlConnection connection, User user)
-        {
-            connection.Update(user);
-            Console.WriteLine("Atualizado com sucesso!");
-        }
-
-        static void UpdateUser(SqlConnection connection, int id)
-        {
-            var user = connection.Get<User>(id);
-            connection.Delete(user);
-
-            Console.WriteLine("Deletado com sucesso!");
-        }
-
     }
 }
